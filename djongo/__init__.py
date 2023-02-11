c09255269ea10bd2b0024b4b11e528e807b085b5
marcus <marcus@us2.ai>
2023-02-11 18:23:53 +0800

    woopsie

--- djongo/__init__.py
+++ djongo/__init__.py
@@ -7,8 +7,4 @@
 #       * Renz Ladia
 #       * thestick613
 
-<<<<<<< HEAD
-__version__ = '1.3.40'
-=======
 __version__ = '1.3.42'
->>>>>>> 6a41e21 (allow datetime to be passed in directly to jsonfield rather than having to serialize it to string)
