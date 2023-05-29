ab4589e265ec859ece30769b14ea7ae47b1d4607
weibeen <weibeen@us2.ai>
2023-05-29 11:28:25 +0800

    Added maxsize to cache, user re.sub for string replacement

--- djongo/utils.py
+++ djongo/utils.py
@@ -1,19 +1,30 @@
+import re
+import functools
+
+
+encode_dict = {
+    ".": "\\u002e",
+    "\$": "\\u0024",
+    "\\": "\\\\",
+}
+encode_regex = re.compile("|".join(map(re.escape, encode_dict.keys())))
+
+
+@functools.lru_cache(maxsize=1024)
 def encode_key(s: str):
-    return s.replace("\\", "\\\\").replace("\$", "\\u0024").replace(".", "\\u002e")
+    if isinstance(s, int):
+        return str(s)
+    elif isinstance(s, str):
+        return encode_regex.sub(lambda match: encode_dict[match.group()], s)
+    else:
+        return s
 
 
-encode_keys_cache = {}
 def encode_keys(obj, is_key=False):
     if is_key:
-        if obj in encode_keys_cache:
-            return encode_keys_cache[obj]
-        if isinstance(obj, int):
-            encode_keys_cache[obj] = str(obj)
-        elif isinstance(obj, str):
-            encode_keys_cache[obj] = encode_key(obj)
-        else:
-            return obj
-        return encode_keys_cache[obj]
+        if isinstance(obj, (int, str)):
+            return encode_key(obj)
+        return obj
     if isinstance(obj, dict):
         return {encode_keys(k, True): encode_keys(v) for k, v in obj.items()}
     if isinstance(obj, list):
@@ -21,21 +32,26 @@ def encode_keys(obj, is_key=False):
     return obj
 
 
+decode_dict = {
+    "\\u002e": ".",
+    "\\u0024": "\$",
+    "\\\\": "\\",
+}
+decode_regex = re.compile("|".join(map(re.escape, decode_dict.keys())))
+
+
+@functools.lru_cache(maxsize=1024)
 def decode_key(s: str):
-    return s.replace("\\u002e", ".").replace("\\u0024", "\$").replace("\\\\", "\\")
+    if s.isdigit() or (s.startswith('-') and s[1:].isdigit()):
+        return int(s)
+    else:
+        return decode_regex.sub(lambda match: decode_dict[match.group()], s)
 
 
-decode_keys_cache = {}
 def decode_keys(obj, is_key=False):
     if isinstance(obj, str):
         if is_key:
-            if obj in decode_keys_cache:
-                return decode_keys_cache[obj]
-            if obj.isdigit() or (obj.startswith('-') and obj[1:].isdigit()):
-                decode_keys_cache[obj] = int(obj)
-            else:
-                decode_keys_cache[obj] = decode_key(obj)
-            return decode_keys_cache[obj]
+            return decode_key(obj)
         return obj
     if isinstance(obj, dict):
         return {decode_keys(k, True): decode_keys(v) for k, v in obj.items()}
